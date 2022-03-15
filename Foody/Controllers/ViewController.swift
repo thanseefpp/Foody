374950bdc17f810d531cbb9ef6be4934c130ef8a
Thanseef <androtsf@gmail.com>
2022-03-15 10:24:29 +0530

    project initialised

---
+++ Foody/Controllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Foody
+//
+//  Created by THANSEEF on 15/03/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
